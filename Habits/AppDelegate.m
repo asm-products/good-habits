4e065258fa6c21d6c9bc4a3a5f5be620596489b8
Michael Forrest <michael.forrest@gmail.com>
2014-07-30 15:22:38 +0200

    Progress during import

--- Habits/AppDelegate.m
+++ Habits/AppDelegate.m
@@ -16,7 +16,7 @@
 #import "Audits.h"
 #import "DataExport.h"
 #import <UIAlertView+Blocks.h>
-
+#import <SVProgressHUD.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -91,8 +91,15 @@
     NSArray * components = [url.absoluteString componentsSeparatedByString:@"goodhabits://import?json="];
     if(components.count > 1){
         [[[UIAlertView alloc] initWithTitle:@"Restore data?" message:@"Restore your data? This action will delete your current data. It might also make any iCloud syncing behave strangely. Proceed with caution." cancelButtonItem:[RIButtonItem itemWithLabel:@"Cancel"] otherButtonItems:[RIButtonItem itemWithLabel:@"Restore Data" action:^{
-            [SVP]
-            [DataExport importDataFromBase64EncodedString:components[1]];
+            [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeBlack];
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                [DataExport importDataFromBase64EncodedString:components[1]];
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [SVProgressHUD dismiss];
+                });
+            });
+
+
         }], nil] show];
         return YES;
     }
